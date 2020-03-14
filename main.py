11f42afe6e5e2ebd668e4119060a2de32196b691
Pikunov A <piskunov.alesha@gmail.com>
2020-03-14 18:11:23 +0700

    logging

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
+import logging
 from selenium import webdriver
+import traceback
+import time
 
-GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
-CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
+logger = logging.getLogger(__name__)
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.binary_location = GOOGLE_CHROME_PATH
+logger.info('preparing...')
+time.sleep(15)
+logger.info('starting...')
 
-browser = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH,
-                           chrome_options=chrome_options)
-browser.close()
+try:
+    GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
+    CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
+
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.binary_location = GOOGLE_CHROME_PATH
+
+    browser = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH,
+                               chrome_options=chrome_options)
+    browser.close()
+except Exception as e:
+    logger.error(f'failed with {e}')
+    logging.error(traceback.format_exc())
+else:
+    logger.info('exit 0')
